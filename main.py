c190529456ca6b9d0b9fe2459a6d46071f6ac522
Nikita <itstudiolinux@gmail.com>
2024-07-28 01:47:11 +0300

    add hw

--- main.py
+++ main.py
@@ -11,5 +11,20 @@ with open('recipe.txt') as file:
         elif files and '|' in files:
             names, quantity, measure = files.split('|')
             cook_book.get(keys).append(dict(ingredient_name=names, quantity=int(quantity), measure=measure))
-print(cook_book)
 
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        for ingredient in cook_book[dish]:
+            new_shop_list_item = dict(ingredient)
+            new_shop_list_item['quantity'] *= person_count
+            if new_shop_list_item['ingredient_name'] not in shop_list:
+                shop_list[new_shop_list_item['ingredient_name']] = new_shop_list_item
+            else:
+                shop_list[new_shop_list_item['ingredient_name']]['quantity'] += new_shop_list_item['quantity']
+        print(shop_list)
+
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+print(cook_book)

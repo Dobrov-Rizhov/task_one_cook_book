d8c41f03768568f1b2b2b648c39c047530ad1747
Nikita <itstudiolinux@gmail.com>
2024-07-30 16:38:34 +0300

    Added a check. Removed the redundant dictionary

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Домавашнее задание №1
 cook_book = {}
 with open('recipe.txt') as file:
     keys = ''
@@ -11,20 +12,33 @@ with open('recipe.txt') as file:
         elif files and '|' in files:
             names, quantity, measure = files.split('|')
             cook_book.get(keys).append(dict(ingredient_name=names, quantity=int(quantity), measure=measure))
+print(cook_book)
 
 
+# Домавашнее задание №2
 def get_shop_list_by_dishes(dishes, person_count):
     shop_list = {}
     for dish in dishes:
-        for ingredient in cook_book[dish]:
-            new_shop_list_item = dict(ingredient)
-            new_shop_list_item['quantity'] *= person_count
-            if new_shop_list_item['ingredient_name'] not in shop_list:
-                shop_list[new_shop_list_item['ingredient_name']] = new_shop_list_item
-            else:
-                shop_list[new_shop_list_item['ingredient_name']]['quantity'] += new_shop_list_item['quantity']
-        print(shop_list)
+        if dish not in cook_book:
+            # Если такого блюда нет в списке то он вывидет сообщение что такого блюда нет в списке
+            print(f'Блюда {dish} нет в списке')
+            continue
+        else:
+            for ingredient in cook_book[dish]:
+                ingredients = ingredient['ingredient_name']
+                if ingredients not in shop_list:
+                    shop_list[ingredients] = {'measure': ingredient['measure'],
+                                              'quantity': ingredient['quantity'] * person_count}
+                else:
+                    shop_list['ingredient_name']['quantity'] += ingredient['quantity'] * person_count
+    # Убрал избыточные копии словаря
+    new_shop_list = {}
+    for key, value in shop_list.items():
+        new_shop_list[key] = value
+    print(new_shop_list)
+
+# Домашняя работа №3
 
 
-get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
-print(cook_book)
+if __name__ == '__main__':
+    get_shop_list_by_dishes(['Запеченный картофель', 'Омлет', 'Утка по-пекински'], 4)

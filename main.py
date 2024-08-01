937716f60e49ddf7f351ed595252b7b5007d30fd
Nikita <itstudiolinux@gmail.com>
2024-08-01 10:55:40 +0300

    home work 3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# Домавашнее задание №1
+import os
+# Домавашнее задание №1.txt
 cook_book = {}
 with open('recipe.txt') as file:
     keys = ''
@@ -31,14 +32,27 @@ def get_shop_list_by_dishes(dishes, person_count):
                                               'quantity': ingredient['quantity'] * person_count}
                 else:
                     shop_list['ingredient_name']['quantity'] += ingredient['quantity'] * person_count
+    print(shop_list)
+
     # Убрал избыточные копии словаря
-    new_shop_list = {}
-    for key, value in shop_list.items():
-        new_shop_list[key] = value
-    print(new_shop_list)
+    # new_shop_list = {}
+    # for key, value in shop_list.items():
+    #     new_shop_list[key] = value
+    # print(new_shop_list)
 
 # Домашняя работа №3
+def homeWorktree(text1, text2, text3):
+    result = []
+    filename = [text1, text2, text3]
+    for name in filename:
+        file_data = {'keys': os.path.basename(name).split('.')[0]}
+        with open(name) as f:
+            contents = f.read()
+            file_data['values'] = contents.replace(os.linesep, ' ').rsplit(',')
+        result.append(file_data)
+    print(result)
 
 
 if __name__ == '__main__':
     get_shop_list_by_dishes(['Запеченный картофель', 'Омлет', 'Утка по-пекински'], 4)
+    homeWorktree('1.txt', '2.txt', '3.txt')
